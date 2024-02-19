b63159284a9f48e734cc7215d3209e91eb346436
AgusZanini <zaniniagustin@gmail.com>
2024-02-19 17:05:00 -0300

    correccion variable de entorno

--- back/config/config.go
+++ back/config/config.go
@@ -5,19 +5,9 @@ import (
 )
 
 var (
-	DBHOST = os.Getenv("INSTANCE_CONNECTION_NAME")
+	DBHOST = os.Getenv("DB_HOST")
 	DBPORT = os.Getenv("DB_PORT")
 	DBNAME = os.Getenv("DB_NAME")
 	DBUSER = os.Getenv("DB_USER")
 	DBPASS = os.Getenv("DB_PASS")
 )
-
-/*
-func DefinePort() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	return port
-}
-*/
