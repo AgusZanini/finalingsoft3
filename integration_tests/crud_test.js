134ca75d0027d76f899893b507938f6098c1a206
AgusZanini <zaniniagustin@gmail.com>
2024-02-20 17:05:12 -0300

    modificados tests de integracion

--- integration_tests/crud_test.js
+++ integration_tests/crud_test.js
@@ -1,11 +1,16 @@
 Feature('crud');
 
-Scenario('test crud operations',  ({ I }) => {
+Scenario('insert_operation',  ({ I }) => {
     I.amOnPage("https://frontend-fuz7myfmqq-uc.a.run.app")
     I.fillField('#num1', '5');
     I.fillField('#num2', '10');
     I.click('#multiply');
     I.see('Resultado: 50', 'h3');
-    I.fillField('#idinsert', '7')
-    I.see('Operación: {"Id":7,"number1":4,"number2":3,"result":12}', 'h3')
 });
+
+Scenario('get_operation', ({ I }) => {
+    I.amOnPage("https://frontend-fuz7myfmqq-uc.a.run.app")
+    I.fillField('#idinsert', '7');
+    I.click('#getbyid');
+    I.see('Operación: {"Id":7,"number1":4,"number2":3,"result":12}', 'h3');
+})
