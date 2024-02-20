f02629a6855f519585c8bc17743b0e3fdd5b52d4
AgusZanini <zaniniagustin@gmail.com>
2024-02-20 17:40:45 -0300

    correcc

--- integration_tests/crud_test.js
+++ integration_tests/crud_test.js
@@ -1,5 +1,6 @@
 Feature('crud');
 
+/*
 Scenario('insert_operation',  ({ I }) => {
     I.amOnPage("https://frontend-fuz7myfmqq-uc.a.run.app")
     I.fillField('#num1', '5');
@@ -7,13 +8,11 @@ Scenario('insert_operation',  ({ I }) => {
     I.click('#multiply');
     I.see('Resultado: 50', {id: 'result'});
 });
+*/
 
-/*
 Scenario('get_operation', ({ I }) => {
     I.amOnPage("https://frontend-fuz7myfmqq-uc.a.run.app")
     I.fillField('#idinsert', '7');
     I.click('#getbyid');
     I.see('Operación: {"Id":7,"number1":4,"number2":3,"result":12}');
 })
-
-*/
